449af5bad456590fe3f4349108d0ffb53d18fc2e
Fernando Wertt <fer19_reta@hotmail.com>
2019-02-24 12:01:29 -0600

    changes
    I add a while true outside the while loop answer != guess

--- guessing_game.py
+++ guessing_game.py
@@ -9,42 +9,45 @@ FERNANDO URIEL WERTT RETA
     
 import random
 record = []
-
 def start_game():
     print(""" 
         ------------------------------------
         Welcome to the Number Guessing Game!
         ------------------------------------
         """)
-    answer = random.randint(1, 10)
-    guess = input("Pick a number between 1 and 10: ")
-    attempts = 1
     
-    while answer != guess:
-        try:
-            guess = int(guess)
-        except ValueError:
-            print("Thats not a number")
-            guess = input("Pick a number between 1 and 10: ")
-            attempts += 1
-        if guess < answer:
-            print("It's higher")
-            guess = input("Pick a number between 1 and 10: ")
-            attempts += 1
-        elif guess > answer:
-            print("It's lower")
-            guess = input("Pick a number between 1 and 10: ")
-            attempts += 1
-    print("NICE! You scored in {} attemps".format(attempts))
-    record.append(attempts)
-    print("Your scores is: {}".format(attempts))
-    score = print("The best record is : {} attempts".format(min(record)))
-    play_again = input("Do you want to play again? ([y]es, [n]o) ")
-    if play_again.lower() == 'y':
-        start_game()
-    else:
-        print("GOOD BYE!!")
-        print("The best record is : {} attempts".format(min(record)))
+    while True:
+        answer = random.randint(1, 10)
+        guess = input("Pick a number  1 and 10: ")
+        attempts = 1
+        while guess != answer:
+            try:
+                guess = int(guess)
+            except ValueError:
+                print("Please only enter numbers ")
+                guess = input("Pick a number  1 and 10: ")
+                attempts += 1
+            else:
+                if guess < answer:
+                    print("It's higher")
+                    guess = input("Pick a number between 1 and 10: ")
+                    attempts += 1
+                elif guess > answer:
+                    print("It's lower")
+                    guess = input("Pick a number between 1 and 10: ")
+                    attempts += 1
+        print("NICE! You scored in {} attemps".format(attempts))
+        record.append(attempts)
+        print("Your scores is: {}".format(attempts))
+        score = print("The best record is : {} attempts".format(min(record)))
+        play_again = input("Do you want to play again? ([y]es, [n]o) ")
+        if play_again.lower() == 'y':
+            start_game()
+        else:
+            print("GOOD BYE!!")
+            print("The best record is : {} attempts".format(min(record)))
+
+
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
